646138a62ec658dbfd028e1df5f6ebe32bfebeb9
Calvin Chan <77070825+FastFusion5@users.noreply.github.com>
2023-03-10 19:38:06 -0800

    Q1 Answer

--- Q1.py
+++ Q1.py
@@ -9,8 +9,8 @@ variables = inputList.split(",");
 numberOfTShirts, TShirtSizes, numOfRequests, requestedSizes = variables
 TShirtSizes = TShirtSizes.split(" ")
 requestedSizes = requestedSizes.split(" ")
-for i in range(len(requestedSizes):
-    if len(requestedSizes = 0)):
+for i in range(len(requestedSizes)):
+    if len(requestedSizes = 0):
         break
     else:
         if len(TShirtSizes) < len(requestedSizes):
@@ -19,11 +19,10 @@ for i in range(len(requestedSizes):
             if requestedSizes[i] == "S" and TShirtSizes[i].contains("M"):
                TShirtSizes.pop(i)
                requestedSizes.pop(i)
-            else if requestedSizes[i] == "M" and TShirtSizes[i].contains("L"):
+            elif requestedSizes[i] == "M" and TShirtSizes[i].contains("L"):
                TShirtSizes.pop(i)
                requestedSizes.pop(i)
-            else if (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("S")) or
-                    (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("L")):
+            elif (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("S")) or (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("L")):
                TShirtSizes.pop(i)
                requestedSizes.pop(i)
             else:
