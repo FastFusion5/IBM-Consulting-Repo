b0e771876ed19bda50fd84537867d6d206348781
Calvin Chan <77070825+FastFusion5@users.noreply.github.com>
2023-03-10 19:36:23 -0800

    Q1 Answer

---
+++ Q1.py
@@ -0,0 +1,31 @@
+inputList = []
+while True:
+    try:
+       line = input()
+    except EOFError:
+        break
+    inputList.append(line)
+variables = inputList.split(",");
+numberOfTShirts, TShirtSizes, numOfRequests, requestedSizes = variables
+TShirtSizes = TShirtSizes.split(" ")
+requestedSizes = requestedSizes.split(" ")
+for i in range(len(requestedSizes):
+    if len(requestedSizes = 0)):
+        break
+    else:
+        if len(TShirtSizes) < len(requestedSizes):
+            print("No")
+        else:
+            if requestedSizes[i] == "S" and TShirtSizes[i].contains("M"):
+               TShirtSizes.pop(i)
+               requestedSizes.pop(i)
+            else if requestedSizes[i] == "M" and TShirtSizes[i].contains("L"):
+               TShirtSizes.pop(i)
+               requestedSizes.pop(i)
+            else if (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("S")) or
+                    (TShirtSizes[i].count("X") > requestedSizes[i].count("X") and TShirtSizes[i].contains("L")):
+               TShirtSizes.pop(i)
+               requestedSizes.pop(i)
+            else:
+               print("No")
+        print("Yes")
